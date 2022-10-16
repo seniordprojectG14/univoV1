d1f6ec723bbd7e05a262f9e25c10c379bf36818d
stephen mcnally <stephen.mcn787@gmail.com>
2022-10-15 20:59:37 -0400

    make it better

--- backend/controllers/univPostControllers.js
+++ backend/controllers/univPostControllers.js
@@ -8,7 +8,7 @@ const multerS3 = require("multer-s3");
 const mongoose = require('mongoose');
 
 
-const Postmodel = require("../models/models/Postmodel");
+const Postmodel = require("../models/Postmodel");
 
 
 const router = express.Router();
